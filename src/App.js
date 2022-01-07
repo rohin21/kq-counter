d6dc220d9be96f98014a0f6651bf0f8ee60753a9
Rohin Pandey <rohinpandey21@gmail.com>
2022-01-07 15:25:28 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.scss";
 
 function App() {
+  const [count, setCount] = useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="app">
+      <div className="button-container">
+        <button onClick={() => setCount(count - 1)}>-</button>
+        <h1
+          className={count > 100 ? "positive" : count < 5 ? "negative" : null}
         >
-          Learn React
-        </a>
-      </header>
+          {count}
+        </h1>
+        <button onClick={() => setCount(count + 1)}>+</button>
+      </div>
     </div>
   );
 }
